060eddc12194a28aa4f05bad50aab5f7a46ee44f
panja84 <panja84@gmail.com>
2019-05-01 16:18:02 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+<<<<<<< HEAD
   title = 'Sample';
+=======
+<<<<<<< HEAD
+  title = 'sample';
+  userName: string;
+  password: string;
+  colr: string;
+  message: string;
+
+  constructor(){
+  }
+
+  validate(): void{
+    if( (this.userName.startsWith("a")) && (this.password.startsWith("b")) ){
+      this.colr ="green";
+      this.message = "Welcome";
+    } else {
+      this.colr ="red";
+      this.message = "Bad User";
+    }
+
+  }
+=======
+  title = 'Sample';
+>>>>>>> 9f0c1f7a6acecc9bc67add58cef58dbea6f77571
+>>>>>>> initial commit
 }
