4e2c7b744a475bbda81e813dccf284a7bdfb65e2
Amey Tuljapurkar <ameyaat@gmail.com>
2019-05-06 09:57:45 +0530

    correcting app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-=======
+import { NgModule,Pipe } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
->>>>>>> f6b64ff25688b61faf05354960d5a12468a43e25
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvHeaderComponent } from './invoice/inv-header/inv-header.component';
 import { SignInComponent } from './home/sign-in/sign-in.component';
 import { InvAddComponent } from './invoice/inv-add/inv-add.component';
 import { HttpClientModule } from '@angular/common/http';
-<<<<<<< HEAD
+import { InvAddParticularComponent } from './invoice/inv-add-particular/inv-add-particular.component';
 import {DropdownModule} from 'primeng/dropdown';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
-import { InvAddParticularComponent } from './invoice/inv-add-particular/inv-add-particular.component';
 
-
->>>>>>> f6b64ff25688b61faf05354960d5a12468a43e25
 @NgModule({
   declarations: [
     AppComponent,
@@ -27,19 +19,17 @@ import { InvAddParticularComponent } from './invoice/inv-add-particular/inv-add-
     SignInComponent,
     InvAddComponent,
     InvAddParticularComponent
+   
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-<<<<<<< HEAD
     HttpClientModule,
     DropdownModule,
-    BrowserAnimationsModule
-=======
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     HttpClientModule
->>>>>>> f6b64ff25688b61faf05354960d5a12468a43e25
   ],
   providers: [],
   bootstrap: [AppComponent]
